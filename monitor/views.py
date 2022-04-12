62e9ac19d50ecf0d2090e3bd8557d16dabda22f2
zakiya adam <zakiyaadan12@gmail.com>
2022-04-12 14:58:05 +0300

    added views login and logout

--- monitor/views.py
+++ monitor/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def register(request):
-  context = {}
-  return render (request, 'acounts/register.html', context)
+    context = {}
+    return render (request, 'acounts/register.html', context)
+
+
+def login_view(request):
+    context = {}
+    return render (request, 'acounts/login.html', context)
+
+
+def logout_view(request):    
+    return redirect ('accounts:login')
