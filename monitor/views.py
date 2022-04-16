ad933248638fa70589c0c461d84a901bdc85ef24
zakiya adam <zakiyaadan12@gmail.com>
2022-04-16 19:34:28 +0300

    installed django-bootstrap

--- monitor/views.py
+++ monitor/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
 from .forms import NewUserForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm 
 
 # Create your views here.
+
+
 def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
@@ -12,10 +14,10 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
+			return redirect("monitor:homepage")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
-	return render (request=request, template_name="main/register.html", context={"register_form":form})
+	return render (request=request, template_name="registration/register.html", context={"register_form":form})
 
 
 
@@ -29,7 +31,7 @@ def login_request(request):
 			if user is not None:
 				login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:homepage")
+				return redirect("monitor:homepage")
 			else:
 				messages.error(request,"Invalid username or password.")
 		else:
@@ -41,4 +43,4 @@ def login_request(request):
 def logout_request(request):
 	logout(request)
 	messages.info(request, "You have successfully logged out.") 
-	return redirect("main:homepage")
+	return redirect("monitor:homepage")
