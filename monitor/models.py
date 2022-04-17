6b44cfe900457e1a8cb4ccf98cee40a29183c044
zakiya adam <zakiyaadan12@gmail.com>
2022-04-17 20:28:50 +0300

    created function post

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,12 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    neighbour= models.ForeignKey(Neighbour, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.TextField()
+    image = CloudinaryField('image')
+
